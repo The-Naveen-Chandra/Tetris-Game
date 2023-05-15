969fe85b8c3818c4e95ff2531796642e89d4917d
Naveen Chandra <the.naveenchandra@gmail.com>
2023-05-15 15:44:20 +0530

    implementing each pieces for the game

--- lib/components/board.dart
+++ lib/components/board.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:tetris_game/components/piece.dart';
 import 'package:tetris_game/components/pixel.dart';
+import 'package:tetris_game/constant/value.dart';
 
 class GameBoard extends StatefulWidget {
   const GameBoard({super.key});
@@ -9,9 +13,41 @@ class GameBoard extends StatefulWidget {
 }
 
 class _GameBoardState extends State<GameBoard> {
-  // grid dimensions
-  int rowLength = 10;
-  int colLenght = 15;
+  // current tetris piece
+  Piece currentPiece = Piece(type: Tetromino.T);
+
+  @override
+  void initState() {
+    super.initState();
+
+    // start the game when the app starts
+    startGame();
+  }
+
+  void startGame() {
+    currentPiece.initializePiece();
+
+    // frame refresh rate
+    Duration frameRate = const Duration(milliseconds: 500);
+    gameLoop(frameRate);
+  }
+
+  // game loop
+  void gameLoop(Duration frameRate) {
+    Timer.periodic(
+      frameRate,
+      (timer) {
+        setState(() {
+          // move the piece down
+          currentPiece.movePiece(Direction.down);
+        });
+      },
+    );
+  }
+
+  // check for collision in a future position
+  // return true -> there is collision
+  // return false -> there is no collision
 
   @override
   Widget build(BuildContext context) {
@@ -22,10 +58,19 @@ class _GameBoardState extends State<GameBoard> {
         physics: const NeverScrollableScrollPhysics(),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: rowLength),
-        itemBuilder: (context, index) => Pixel(
-          color: Colors.grey[900],
-          child: index,
-        ),
+        itemBuilder: (context, index) {
+          if (currentPiece.position.contains(index)) {
+            return Pixel(
+              color: Colors.yellow,
+              child: index,
+            );
+          } else {
+            return Pixel(
+              color: Colors.grey[900],
+              child: index,
+            );
+          }
+        },
       ),
     );
   }
